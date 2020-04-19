9a6e45f8dacf8f3e96a967e9a9fb40807bb07eeb
gesangtome <gesangtome@foxmail.com>
2020-04-19 23:43:41 +0800

    Revert "tweak ci script"
    This reverts commit 980557f6778d35fc3e9f661121793bf502dfbac8.

--- ci-build.sh
+++ ci-build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
-sudo apt install -y build-essential uuid-dev iasl git nasm gcc-aarch64-linux-gnu
+sudo apt update
+sudo apt install build-essential uuid-dev iasl git nasm python3-distutils gcc-aarch64-linux-gnu
 curdir="$PWD"
 cd ..
 git clone https://github.com/tianocore/edk2.git --recursive
